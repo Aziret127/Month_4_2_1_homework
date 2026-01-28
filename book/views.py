3c1561515b66a37f1dfe7a6d18af926fec8d3894
Aziret127 <utachi86@gmail.com>
2026-01-28 21:34:09 +0600

    выполнил дз

--- book/views.py
+++ book/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# View for the /book/ URL
 def index_view(request):
    if request.method == "GET":
          return HttpResponse("Жизнь - это то, что с тобой происходит, пока ты строишь планы.")
